1f29edd9fb8cff6f21ce04447a7b46f55eaddf01
William Oliveira de Lagos <william.lagos@live.com>
2017-10-23 17:59:06 -0200

    Added new base for React app

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'build')))
 
 var port = process.env.PORT || 5000
 app.listen(port)
-console.log('server started ' + port)
+console.log('Server started on ' + port)
