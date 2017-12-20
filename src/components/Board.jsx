7c92fbfac9bcd9ee81a117d69318f137bf3cd870
William Oliveira de Lagos <william.lagos@live.com>
2017-12-20 02:38:58 -0200

    Added new sidebar for navigation

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
 import axios from 'axios';
 
 class Board extends Component {
@@ -22,9 +23,18 @@ class Board extends Component {
   render() {
     return (
       <div>
-        <br/><img className="img-fluid" alt="Drinks" src="./drinks.png"/><br/><br/>
-        <h1>Bem-vindo ao Vupit App</h1>
-        <p>No momento não há bebidas para você escolher.</p>
+        <Menu>
+          <a id="beer" className="menu-item text-left" href="/"><h4>Cervejas</h4></a><br/><br/>
+          <a id="craft" className="menu-item text-left" href="/"><h4>Artesanais</h4></a><br/><br/>
+          <a id="distilled" className="menu-item text-left" href="/"><h4>Destilados</h4></a><br/><br/>
+          <a id="sparkling" className="menu-item text-left" href="/"><h4>Espumantes</h4></a><br/><br/>
+          <a id="wine" className="menu-item text-left" href="/"><h4>Vinhos</h4></a><br/><br/>
+        </Menu>
+        <div>
+          <br/><img className="img-fluid" alt="Drinks" src="./drinks.png"/><br/><br/>
+          <h1>Bem-vindo ao Vupit App</h1>
+          <p>No momento não há bebidas para você escolher.</p>
+        </div>
       </div>
     )
     // return (<h1> Hello World at {this.key} on {this.api} </h1>);
