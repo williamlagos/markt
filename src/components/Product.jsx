3a91439fce6a3030570e2fae0374c5c729aa298c
William Oliveira de Lagos <william.lagos@live.com>
2018-01-09 05:07:21 -0200

    Added product view

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -31,13 +31,14 @@ class Product extends Component {
     if(this.state.products.length !== 0) {
       let products = this.state.products;
       return products.map((product) => {
+        let p = product.key;
         return (
-          <div className="col-md-4" style={{ marginBottom: '5px', marginTop: '5px' }}>
+          <div className="col-md-3" style={{ marginBottom: '5px', marginTop: '5px' }}>
             <div className="card">
-              <img className="card-img-top" src={product.key} alt="Provider"/>
+              <a style={{ width: '100%' }} href="#product"><img className="card-img-top img-fluid" style={{ width: '30px' }} src={p.photo} alt="Provider"/></a>
               <div className="card-body">
-                <h5 className="card-title">{product.value}</h5>
-                <p className="card-text">{product.quantity}</p>
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">{p.description}</p>
               </div>
             </div>
           </div>
