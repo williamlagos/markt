5beb23e6bdfa009c45acf2eb8ca1f011a634f12b
William Oliveira de Lagos <william.lagos@icloud.com>
2020-06-21 21:29:26 -0300

    Added capacitor config and moved public folder to www

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, '../public')))
+app.use(serveStatic(path.join(__dirname, '../www')))
 
 var port = process.env.PORT || 5000
 app.listen(port)
